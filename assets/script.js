4c675be57d7f490f3300ee911d5102c3e9e3e7d0
Cole <ce.kasabian@gmail.com>
2023-02-21 20:50:03 -0800

    final changes

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,43 @@ var searchBtn = document.getElementById("search");
 var cardTitle = document.querySelector(".card-title");
 var cardText = document.querySelector(".card-text");
 var apiKey = "a5c580663ddccd88b1db1d7c94e30d3e";
+var card = document.querySelector(".screen");
+var cityInput = document.querySelector(".citySearch")
+var recentCities = []
+// Jquery uses just the $ instead of document 
+var recent = $("#recent")
+var clear = $("#clearHistory")
 
-function apiGrab() {
+$(document).ready(function(){
+  var recentCity = JSON.parse(localStorage.getItem("cities"))
+  if(recentCity){
+    recentCities = recentCity
+  }
+  renderCities()
+  console.log(recentCity)
+})
+
+function clearHistory(){
+  recent.empty()
+}
+
+function renderCities(){
+  clearHistory()
+    for(var i = 0; i<recentCities.length; i++){
+    console.log(recentCities[i])
+    var btnCity =  $("<button></button>")
+    btnCity.text(recentCities[i])
+    btnCity.on("click", (e) => {
+      apiGrab(e.currentTarget.textContent)
+      card.classList.remove("hide")
+    })
+    recent.append(btnCity)
+  }
+}
+
+function apiGrab(cityName) {
   let data = fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=" + apiKey
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${apiKey}`
   )
     .then(function (response) {
       return response.json();
@@ -45,17 +78,27 @@ function getCity(lat, lon) {
         var humidityEl = $(`#humidity-${cardIndex}`)
         var tempEl = $(`#temp-${cardIndex}`)
         var tempFeel = $(`#tempFeel-${cardIndex}`)
-        humidityEl.text(dataList[i].main.humidity)
-        tempEl.text(dataList[i].main.temp)
-        tempFeel.text(weatherData.main.feels_like)
+        humidityEl.text("Humidity: " + dataList[i].main.humidity)
+        tempEl.text("Temperature: " + dataList[i].main.temp)
+        tempFeel.text("Feels Like: " + weatherData.main.feels_like)
         iconEl.attr("src",`http://openweathermap.org/img/w/${dataList[i].weather[0].icon}.png`)
         dateEl.text(date.toLocaleDateString("en-US"))
         console.log(dataList[i]);
         cardIndex++
       }
     });
-    
-
 }
 
-searchBtn.addEventListener("click", apiGrab);
+searchBtn.addEventListener("click", function(){
+  apiGrab(cityInput.value)
+  card.classList.remove("hide")
+  recentCities.push(cityInput.value)
+  localStorage.setItem("cities", JSON.stringify(recentCities))
+  renderCities();
+});
+
+clear.on("click", function(){
+  clearHistory()
+  recentCities = []
+  localStorage.setItem("cities", JSON.stringify(recentCities))
+})
